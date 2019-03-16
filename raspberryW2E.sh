08e04a0584e25c40c051acfe13b75fd82bdacfe7
leo <leo-ryu@outlook.com>
2019-03-16 12:11:21 +0800

    change birdge ip

--- raspberryW2E.sh
+++ raspberryW2E.sh
@@ -4,5 +4,5 @@
 ### sudo vim /etc/sysctl.conf
 ### + net.ipv4.ip_forward = 1
 ### sudo vim /etc/dnsmasq.conf
-### + dhcp-range=192.168.1.50,192.168.1.150,255.255.255.0,12h
-sudo brctl addbr br-lan && brctl addif br-lan eth0 && ifconfig br-lan 192.168.1.1 up && ifconfig eth0 0.0.0.0 up && iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+### + dhcp-range=192.168.111.50,192.168.111.150,255.255.255.0,12h
+sudo brctl addbr br-lan && brctl addif br-lan eth0 && ifconfig br-lan 192.168.111.1 up && ifconfig eth0 0.0.0.0 up && iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
